9bca02b89a4549cc164fafad06051cf6d0e2b65c
Soham Sheth <sohamashishsheth@gmail.com>
2024-10-15 08:50:57 -0500

    Added brute force algo for searching largest element in array

---
+++ 3.Data Structures/2.Arrays/1.Largest Element  in an array.py
@@ -0,0 +1,19 @@
+
+
+#Brute force solution 
+
+
+def main(a):
+    largest = a[0]
+    n = len(a)
+    for i in range (0,n):
+        if a[i]>largest:
+            largest = a[i]
+    
+    return largest 
+
+result = main([2,3,4,6,1,1,5,3,1])
+print(result)
+
+
+    
